a7c923d3f16fd79492ff593f5597ed13b93b5ba6
Brewster Malevich <bmalevich@rhg.com>
2025-12-12 18:01:24 -0800

    Preliminary example script for mortality transformation and projection

--- examples/mortality_cmip5/mortality.py
+++ examples/mortality_cmip5/mortality.py
@@ -1,5 +1,5 @@
 %pip install muuttaa==0.1.1
-%pip install git+https://github.com/brews/ideal-succotash@d92c330985bb04cce0af2df11f67403b86da48b2 --no-deps
+%pip install git+https://github.com/brews/ideal-succotash@b080eb37e184f1e3ef8883ccbe24691ae015aee1 --no-deps
 
 import datetime
 import os
@@ -8,11 +8,11 @@ import uuid
 from dask_gateway import GatewayCluster
 import muuttaa
 import numpy as np
-import pandas as pd
 import xarray as xr
-from xhistogram.xarray import histogram
 
 import ideal_succotash.mortality.transformation as transformation
+import ideal_succotash.mortality.projection as projection
+
 
 JUPYTER_IMAGE = os.environ.get("JUPYTER_IMAGE")
 UID = str(uuid.uuid4())
@@ -54,34 +54,59 @@ effect_params = xr.Dataset(
 cmip = xr.open_datatree(CMIP_URI, engine="zarr", chunks={}).chunk(
     {"time": 365 * 50, "lat": 90, "lon": 90}
 )
-
 test_ds = cmip["rcp45/ACCESS1-0"].ds
 
 
-# TODO: This scales too large for dask graph when on long timeseries and global hierid regions.
-# Need dask cluster for these climate transformations.
-with GatewayCluster(worker_image=JUPYTER_IMAGE, scheduler_image=JUPYTER_IMAGE, profile="micro") as cluster:
-    client = cluster.get_client()
-    print(client.dashboard_link)
-    cluster.scale(50)
+# Need dask cluster for these climate transformations and projection.
+cluster = GatewayCluster(worker_image=JUPYTER_IMAGE, scheduler_image=JUPYTER_IMAGE)
+client = cluster.get_client()
+print(client.dashboard_link)
+cluster.scale(50)
 
-    transformed = muuttaa.apply_transformations(
-        test_ds.drop_encoding(),
-        regionalize=segment_weights,
-        strategies=[
-            transformation.make_climtas,
-            transformation.make_tas_20yrmean_annual_histogram,
-        ],
-    )
-    transformed = transformed.chunk({"region": "auto", "year": -1, "tas_bin": -1})
+# Regionalization and transformation.
+transformed = muuttaa.apply_transformations(
+    test_ds.drop_encoding(),
+    regionalize=segment_weights,
+    strategies=[
+        transformation.make_climtas,
+        transformation.make_tas_20yrmean_annual_histogram,
+    ],
+)
 
-    transformed = (
-        transformed
-        .assign_coords(tas_bin=(transformed["tas_bin"] - 273.15))
-        .assign(climtas=(transformed["climtas"] - 273.15))
-        .sel(year=slice(1980, 2099)) # Years outside this have NA due to climtas rolling operations.
-    )
-    # TODO: Unit conversion should happen in transformation.
+# Some extra post-processing that was part of the carb projection.
+# TODO: Unit conversion should happen in transformation.
+transformed = (
+    transformed
+    .assign_coords(tas_bin=(transformed["tas_bin"] - 273.15))
+    .assign(climtas=(transformed["climtas"] - 273.15))
+    .sel(year=slice(2010, 2099)) # Match socioecon data range.
+)
+transformed = transformed.compute()
+out_path = f"{os.environ['CIL_SCRATCH_PREFIX']}/{os.environ['JUPYTERHUB_USER']}/{UID}/transformed.zarr"
+transformed.chunk({"region": "auto", "year": -1, "tas_bin": -1}).unify_chunks().drop_encoding().to_zarr(out_path, mode="w")
+print(out_path)
 
-    transformed = transformed.compute()
+
+transformed = xr.open_zarr(out_path)
+# Merging and rechunking projection input  here explicitly because this can be sensitive.
+projection_input = xr.merge([transformed, effect_params])
+projection_input = projection_input.chunk(
+    {"region": 5000, "year": -1, "tas_bin": -1, "age_cohort": -1, "degree": -1, "model": 1, "ssp": 1},
+).unify_chunks().persist()
+
+# Project mortality effects.
+projected = muuttaa.project(
+    projection_input,
+    model=projection.mortality_effect_model_gamma,
+    parameters=xr.Dataset({}),
+)
+projected = projected.chunk({"region": "auto", "year": -1, "age_cohort": -1})
+projected.persist()
+
+out_path = f"{os.environ['CIL_SCRATCH_PREFIX']}/{os.environ['JUPYTERHUB_USER']}/{UID}/projected.zarr"
+projected.drop_encoding().to_zarr(out_path, mode="w")
+print(out_path)
+
+cluster.scale(0)
+cluster.shutdown()
 
